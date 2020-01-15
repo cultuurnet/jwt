271acb7d0520d4b0307489bc40b3c5784a295c03
Bert Ramakers <bert@madewithlove.be>
2020-01-15 14:28:08 +0100

    III-3197 Move code from symfony-security-jwt

---
+++ src/Symfony/Authentication/JwtUserToken.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace CultuurNet\UDB3\Jwt\Symfony\Authentication;
+
+use Lcobucci\JWT\Token as Jwt;
+use Symfony\Component\Security\Core\Authentication\Token\AbstractToken;
+
+class JwtUserToken extends AbstractToken
+{
+    /**
+     * @var Jwt
+     */
+    private $jwt;
+
+    /**
+     * @param Jwt $jwt
+     * @param bool $authenticated
+     */
+    public function __construct(Jwt $jwt, $authenticated = false)
+    {
+        parent::__construct();
+        $this->setAuthenticated($authenticated);
+        $this->jwt = $jwt;
+    }
+
+    /**
+     * @return Jwt
+     */
+    public function getCredentials()
+    {
+        return $this->jwt;
+    }
+}
